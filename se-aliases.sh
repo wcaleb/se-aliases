35149e80722e96999086458e87227d146061cb69
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2012-09-06 16:31:11 -0500

    Initial commit

---
+++ se-aliases.sh
@@ -0,0 +1,11 @@
+# http://ruslanspivak.com/2010/06/02/urlencode-and-urldecode-from-a-command-line/
+
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+
+function scholar()
+{
+	QUERY=$(urlencode "$1")
+	open "http://scholar.google.com/scholar?hl=en&q=`echo $QUERY`"
+}
+
