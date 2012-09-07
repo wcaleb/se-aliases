036595075f1580bc9e022ec38976e0f16a922639
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2012-09-06 19:36:33 -0500

    Deleted unnecessary echo and ()

--- se-aliases.sh
+++ se-aliases.sh
@@ -3,9 +3,9 @@
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
 
-function scholar()
+function scholar
 {
 	QUERY=$(urlencode "$1")
-	open "http://scholar.google.com/scholar?hl=en&q=`echo $QUERY`"
+	open "http://scholar.google.com/scholar?hl=en&q=$QUERY"
 }
 
